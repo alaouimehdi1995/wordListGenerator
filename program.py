5dd06a080a28f404af94422be54cf6144f3b2ea5
Mehdi ALAOUI <alaoui.mehdi.1995@gmail.com>
2017-04-20 02:56:14 +0100

    Code reviewed

--- program.py
+++ program.py
@@ -1,11 +1,10 @@
 import sys
 import re
+
+
 files={'input':None,'output':None}
 types={'lower':False,'upper':False,'number':False}
-
-Min=3
-Max=4
-chars=['a','b','c','d','e']
+limits={'min':3,'max':4}
 '''
 Args:
 -a:minuscule
@@ -13,60 +12,93 @@ Args:
 -n:nombre
 -o,--output
 -i, --input
-
+-m,--min:min
+-M,--max:max
 '''
 #def write():#doit écrire tout les 1000 mots
 def getArgs():
 	args=sys.argv[1:]
-	if(len(args)>0):
+	if(len(args)>0): #if there is arguments
+		#Extracting input,output,min and max arguments
 		if "-o" in args or "--output" in args:
 			files['output']=args[args.index('-o')+1] if '-o' in args else args[args.index('--output')+1]
 		if "-i" in args or "--input" in args:
 			files['input']=args[args.index('-i')+1] if '-i' in args else args[args.index('--input')+1]
+		if "-m" in args or "--min" in args:
+			limits['min']=int(args[args.index('-m')+1]) if '-m' in args else int(args[args.index('--min')+1])
+		if "-M" in args or "--max" in args:
+			limits['max']=int(args[args.index('-M')+1]) if '-M' in args else int(args[args.index('--max')+1])
+		
+		#extracting -a,-A,-n and their combinations using regex
 		result = re.findall('-(a|A|n)(a|A|n)?(a|A|n)?', " ".join(args), re.DOTALL)
-		result= [list(e) for e in result]
+		result = [list(e) for e in result]
 		resultArray=[]
 		for e in result:
 			resultArray+=e
 		
-		
 		types['lower']=('a' in resultArray)
 		types['upper']=('A' in resultArray)
 		types['number']=('n' in resultArray)
-		print(files)
-		print(types)
+		limits['max']=limits['min'] if limits['max']<limits['min'] else limits['max']	#in case min > max
+		
+		#Creating the authorized characters final list
+
+		authorizedChars=[]
+		if(files['input']):
+			try:
+				authorizedChars=''.join(open(files['input'],'r').readlines())
+			except:
+				print("Input file not found or not authorized, exiting..")
+				exit(0)
+			authorizedChars=list(authorizedChars)
+			authorizedChars=[e for e in authorizedChars if (e!=" " and e!="")]
+
+		else:
+			if(types['lower']):
+				for e in range(97,123): #97 is 'a' 's ASCII, 122 is for 'z'
+					authorizedChars.append(chr(e))
+			if(types['upper']):
+				for e in range(65,91):
+					authorizedChars.append(chr(e))
+			if(types['number']):
+				for e in range(48,58):
+					authorizedChars.append(chr(e))
+		authorizedChars=sorted(set(authorizedChars))
+		print(authorizedChars)
+		return authorizedChars
+	else:
+		print("There is no arguments, exiting..")
+		exit(0)
 		
 
 
 
 
+authorizedChars=getArgs()
 
-getArgs()
-actualSize=Min
+actualSize=limits['min']
 string=[]
 index=[]
-while(actualSize<=Max):
-	string=[]
-	index=[]
-	for i in range(0,actualSize):
-		string.append(chars[0])
-		index.append(0)
+
+while(actualSize<=limits['max']):
+	string=actualSize*[authorizedChars[0]]
+	index=actualSize*[0]
 	#string=['a','a','a'], index=[0,0,0]
-	#print(''.join(string))
+	print(''.join(string))
 	currentChar=actualSize-1
 	currentIndex=0
 
 	#initialisé
 	while(currentChar>=0):
 		
-		if(index[currentChar]<len(chars)-1):
+		if(index[currentChar]<len(authorizedChars)-1):
 			index[currentChar]+=1
-			string[currentChar]=chars[index[currentChar]]
-			#print(''.join(string))
+			string[currentChar]=authorizedChars[index[currentChar]]
+			print(''.join(string))
 			currentChar=len(string)-1
 		else:
 			index[currentChar]=0
-			string[currentChar]=chars[index[currentChar]]
+			string[currentChar]=authorizedChars[index[currentChar]]
 			currentChar-=1
 	actualSize+=1
 				
@@ -76,11 +108,11 @@ while(actualSize<=Max):
 Algorithme:
 taille du mot=min
 tant que la taille <max:
-	on initialise un tableau string de la taille actuelle, entierement fait par chars[0]
+	on initialise un tableau string de la taille actuelle, entierement fait par authorizedChars[0]
 	aussi un tableau index de la taille actuelle, entierement fait de 0
 	le caractère courant est le dernier,
 	tant qu'on n'a pas atteint le premier ( la fin)
-	si on n'a pas encore fini chars pour le caractère actuel, on incrémente l'index du caractère actuel, on change le caractère, et on retourne à la fin de la chaine
+	si on n'a pas encore fini authorizedChars pour le caractère actuel, on incrémente l'index du caractère actuel, on change le caractère, et on retourne à la fin de la chaine
 	si on atteint la limite, on remet l'index actuel à 0, on change le caractère et le caractère courant devient celui qui le précède
 '''
 			
